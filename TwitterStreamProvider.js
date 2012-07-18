9038103f7362e847f3902455d388b359a286a334
Dan Palmer <dan.palmer@me.com>
2012-07-18 23:07:07 +0100

    plugin now creates itself. can return the authentication options.

--- TwitterStreamProvider.js
+++ TwitterStreamProvider.js
@@ -1,1 +1,28 @@
-"This is some test content";
+(function(R) {
+	var TwitterStreamProvider = function() {
+
+	};
+
+	/**
+		Provides details about the type of authentication that will be performed
+		so that the application may display an appropriate interface.
+		@return object in the form given here
+	*/
+	TwitterStreamProvider.prototype.authRequirements = function() {
+		return {
+			authType: "credentials",
+			field1: {
+					"name": "Username",
+					"type": "text",
+					"identifier": "username"
+			},
+			field2:	{
+				"name": "Password",
+				"type": "secure",
+				"identifier": "password"
+			}
+		};
+	};
+
+	return new TwitterStreamProvider();
+})
